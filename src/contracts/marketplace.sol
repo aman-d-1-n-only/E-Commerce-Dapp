d23c2c86ad12298c0ae61fb3c8f123771db52ddd
aman-d-1-n-only <abhitheone456@gmail.com>
2021-01-30 22:16:30 +0530

    Added a way to add products and tested that.

--- src/contracts/marketplace.sol
+++ src/contracts/marketplace.sol
@@ -3,7 +3,51 @@ pragma solidity ^0.5.0;
 contract Marketplace {
     string public name;
 
+    //Stores the no of products we have on blockchain in our mapping
+    uint256 public productCount = 0;
+
+    //to store the list of products : as this is state variable so products added in this mapping is added to blockchain
+    mapping(uint256 => Product) public products;
+
+    //Custom Data Structure for storing the object
+    struct Product {
+        uint256 id;
+        string name;
+        uint256 price;
+        address owner;
+        bool purchase;
+    }
+
+    event ProductCreated(
+        uint256 id,
+        string name,
+        uint256 price,
+        address owner,
+        bool purchase
+    );
+
     constructor() public {
         name = "Hello World !! ";
+    }
+
+    function createProduct(string memory _name, uint256 _price) public {
+        //Make sure parameters are correct
+        require(bytes(_name).length > 0);
+        require(_price > 0);
+
+        //Increment productCount
+        productCount++;
+
+        //Create the product
+        products[productCount] = Product(
+            productCount,
+            _name,
+            _price,
+            msg.sender,
+            false
+        );
+
+        //Emiiting a event that product is stored on blockchain
+        emit ProductCreated(productCount, _name, _price, msg.sender, false);
     }
 }
--- test/marketplace.test.js
+++ test/marketplace.test.js
@@ -1,8 +1,11 @@
-const { assert } = require("chai");
-
 const Marketplace = artifacts.require('./Marketplace');
 
-contract('Marketplace', ( accounts ) => {
+//required to use chai with promises
+require('chai')
+    .use(require('chai-as-promised'))
+    .should()
+
+contract('Marketplace', ( [deployer , seller , buyer ]) => {
     let marketplace;
 
     before(async () => {
@@ -21,6 +24,35 @@ contract('Marketplace', ( accounts ) => {
         it('has a name', async () => {
             const name = await marketplace.name();
             assert.equal( name , "Hello World !! " )
+        })
+    })
+    
+    describe('products', async () => {
+
+        let result, productCount 
+        before(async () => {
+            //In Ethereum ether is stored in wei . 1 ether = 10^18 wei 
+            result = await marketplace.createProduct('iPhone X', web3.utils.toWei('1', 'Ether'), { from: seller })//{ from : }metadata
+            productCount = await marketplace.productCount()
+        })
+
+
+        it('create product', async () => {
+
+            //SUCCESS CASE : If product is created then what we are creating is what strring on blockchain
+            assert.equal(productCount, 1)
+            const event = result.logs[0].args 
+            assert.equal(event.id.toNumber(), productCount.toNumber(), 'id is correct');
+            assert.equal(event.name , 'iPhone X' , 'name is correct');
+            assert.equal(event.price , '1000000000000000000' , 'price is correct');
+            assert.equal(event.owner ,seller  , 'owner is correct');
+            assert.equal(event.purchase, false, 'purchase is correct');
+            
+            //Failure Case : If the product parameters are not fulfilled then smart contract is reacting in a way it should be
+            await marketplace.createProduct('', web3.utils.toWei('1', 'Ether'), { from: seller }).should.be.rejected
+            await marketplace.createProduct('iPhone X', 0 , { from: seller }).should.be.rejected
+
+
         })
     })
 })
