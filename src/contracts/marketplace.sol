30f9a2895667937c16ed7f062b921c6d043e0f61
aman-d-1-n-only <abhitheone456@gmail.com>
2021-01-31 00:35:58 +0530

    Created a function to buy product and tested buying of product

--- src/contracts/marketplace.sol
+++ src/contracts/marketplace.sol
@@ -14,7 +14,7 @@ contract Marketplace {
         uint256 id;
         string name;
         uint256 price;
-        address owner;
+        address payable owner;
         bool purchase;
     }
 
@@ -22,7 +22,15 @@ contract Marketplace {
         uint256 id,
         string name,
         uint256 price,
-        address owner,
+        address payable owner,
+        bool purchase
+    );
+
+    event ProductPurchased(
+        uint256 id,
+        string name,
+        uint256 price,
+        address payable owner,
         bool purchase
     );
 
@@ -49,5 +57,46 @@ contract Marketplace {
 
         //Emiiting a event that product is stored on blockchain
         emit ProductCreated(productCount, _name, _price, msg.sender, false);
+    }
+
+    function purchaseProduct(uint256 _id) public payable {
+        //Fetch the product and create its copy
+        Product memory _product = products[_id];
+
+        //Fetch the owner
+        address payable _seller = _product.owner;
+
+        //Make sure the product has valid id
+        require(_product.id > 0 && _product.id <= productCount);
+
+        //Make sure there are enough ether in transaction
+        require(msg.value >= _product.price);
+
+        //Make Sure product is not purchase yet
+        require(!_product.purchase);
+
+        //Make buyer != seller
+        require(_seller != msg.sender);
+
+        //Transer the ownership
+        _product.owner = msg.sender;
+
+        //Purchase it
+        _product.purchase = true;
+
+        //Update the products
+        products[_id] = _product;
+
+        //Pay the seller
+        address(_seller).transfer(msg.value);
+
+        //Trigger event
+        emit ProductPurchased(
+            productCount,
+            _product.name,
+            _product.price,
+            msg.sender,
+            true
+        );
     }
 }
--- test/marketplace.test.js
+++ test/marketplace.test.js
@@ -1,3 +1,5 @@
+const { assert } = require('chai');
+
 const Marketplace = artifacts.require('./Marketplace');
 
 //required to use chai with promises
@@ -49,8 +51,8 @@ contract('Marketplace', ( [deployer , seller , buyer ]) => {
             assert.equal(event.purchase, false, 'purchase is correct');
             
             //Failure Case : If the product parameters are not fulfilled then smart contract is reacting in a way it should be
-            await marketplace.createProduct('', web3.utils.toWei('1', 'Ether'), { from: seller }).should.be.rejected
-            await marketplace.createProduct('iPhone X', 0 , { from: seller }).should.be.rejected
+            await marketplace.createProduct('', web3.utils.toWei('1', 'Ether'), { from: seller }).should.be.rejected //name parameter
+            await marketplace.createProduct('iPhone X', 0 , { from: seller }).should.be.rejected// price parameter
 
 
         })
@@ -64,6 +66,51 @@ contract('Marketplace', ( [deployer , seller , buyer ]) => {
             assert.equal(product.owner ,seller  , 'owner is correct');
             assert.equal(product.purchase, false, 'purchase is correproduct');
 
+        })
+
+        it('sells the product', async () => {
+
+            //seller balance
+            let sellerOldBalance 
+            sellerOldBalance = await web3.eth.getBalance(seller);
+            sellerOldBalance = new web3.utils.BN(sellerOldBalance);
+
+            //SUCCESS : Buyer make purchase
+            const result = await marketplace.purchaseProduct(productCount, { from: buyer, value: web3.utils.toWei('1', 'Ether') });
+
+            //Check logs
+            const event = result.logs[0].args
+            assert.equal(event.id.toNumber(), productCount.toNumber(), 'id is correct');
+            assert.equal(event.name , 'iPhone X' , 'name is correct');
+            assert.equal(event.price , '1000000000000000000' , 'price is correct');
+            assert.equal(event.owner , buyer , 'owner is correct');
+            assert.equal(event.purchase, true, 'purchase is correproduct');
+            
+            //Check seller received the fund
+            let sellerNewBalance 
+            sellerNewBalance = await web3.eth.getBalance(seller)
+            sellerNewBalance = new web3.utils.BN(sellerNewBalance)
+
+            let price 
+            price = web3.utils.toWei('1', 'Ether');
+            price = new web3.utils.BN(price);
+
+            const expectedBalance = sellerOldBalance.add(price);
+            assert.equal( sellerNewBalance.toString() , expectedBalance.toString() )
+
+            //Failure : if the product is not valid or buyable
+            //not having vaild id
+            await marketplace.purchaseProduct(99, { from: buyer, value: web3.utils.toWei('1', 'Ether') }).should.be.rejected
+
+            //Not having enought ether
+            await marketplace.purchaseProduct(1, { from: buyer, value: web3.utils.toWei('0.5', 'Ether') }).should.be.rejected
+            
+            //not buyed twice
+            await marketplace.purchaseProduct( productCount , { from: deployer , value: web3.utils.toWei('1', 'Ether') }).should.be.rejected
+
+            //buyer = seller
+            await marketplace.purchaseProduct( 1 , { from: seller, value: web3.utils.toWei('1', 'Ether') }).should.be.rejected
+            
         })
     })
 })
