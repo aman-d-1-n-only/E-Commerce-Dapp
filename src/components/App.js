096943351059709aca488f721b4403ac078e06ac
aman-d-1-n-only <abhitheone456@gmail.com>
2021-02-04 23:59:13 +0530

    Instanciated Web3 object and connected to our blockchain wit

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,43 @@
 import React, { Component } from 'react';
-import logo from '../logo.png';
+import Web3 from 'web3'
 import './App.css';
 
 class App extends Component {
+
+  //React Component Life Cycle Hook
+  async componentWillMount() {
+    await this.loadWeb3
+    console.log('We are connected to our ethereum blockchain')
+  };
+
+  //Instanciating Web3 with provider exposed by the metamask in a browser
+  async loadWeb3() {
+    window.addEventListener('load', async () => {
+    // Modern dapp browsers...
+    if (window.ethereum) {
+        window.web3 = new Web3(ethereum);
+        try {
+            // Request account access if needed
+            await ethereum.enable();
+            // Acccounts now exposed
+            web3.eth.sendTransaction({/* ... */});
+        } catch (error) {
+            // User denied account access...
+        }
+    }
+    // Legacy dapp browsers...
+    else if (window.web3) {
+        window.web3 = new Web3(web3.currentProvider);
+        // Acccounts always exposed
+        web3.eth.sendTransaction({/* ... */});
+    }
+    // Non-dapp browsers...
+    else {
+        console.log('Non-Ethereum browser detected. You should consider trying MetaMask!');
+    }
+    });
+  }
+
   render() {
     return (
       <div>
@@ -13,32 +48,17 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Dapp University
+            MarketPlace
           </a>
         </nav>
         <div className="container-fluid mt-5">
           <div className="row">
             <main role="main" className="col-lg-12 d-flex text-center">
               <div className="content mr-auto ml-auto">
-                <a
-                  href="http://www.dappuniversity.com/bootcamp"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <img src={logo} className="App-logo" alt="logo" />
-                </a>
-                <h1>Dapp University Starter Kit</h1>
+                <h1>Market Place</h1>
                 <p>
-                  Edit <code>src/components/App.js</code> and save to reload.
+                  Earn some money with your used stuff !!!
                 </p>
-                <a
-                  className="App-link"
-                  href="http://www.dappuniversity.com/bootcamp"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  LEARN BLOCKCHAIN <u><b>NOW! </b></u>
-                </a>
               </div>
             </main>
           </div>
