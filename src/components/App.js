aa7e9eefb5c1dfa895c1e14fb0f7a577772cb13a
aman-d-1-n-only <abhitheone456@gmail.com>
2021-03-05 13:10:58 +0530

    fix : Fix the connection of react app with metamask.

--- src/components/App.js
+++ src/components/App.js
@@ -1,42 +1,51 @@
 import React, { Component } from 'react';
+import MarketPlace from '../abis/Marketplace.json';
 import Web3 from 'web3'
 import './App.css';
 
 class App extends Component {
-
-  //React Component Life Cycle Hook
-  async componentWillMount() {
-    await this.loadWeb3
-    console.log('We are connected to our ethereum blockchain')
+  state = {
+    accounts: null,
+    instance: null
   };
 
-  //Instanciating Web3 with provider exposed by the metamask in a browser
+  async componentWillMount() {
+    await this.loadWeb3();
+    console.log('We are connected to our ethereum blockchain.');
+    await this.loadBlockChainData();
+    console.log( this.state.account , this.state.instance )
+  };
+
   async loadWeb3() {
-    window.addEventListener('load', async () => {
-    // Modern dapp browsers...
-    if (window.ethereum) {
-        window.web3 = new Web3(ethereum);
-        try {
-            // Request account access if needed
-            await ethereum.enable();
-            // Acccounts now exposed
-            web3.eth.sendTransaction({/* ... */});
-        } catch (error) {
-            // User denied account access...
-        }
-    }
-    // Legacy dapp browsers...
-    else if (window.web3) {
-        window.web3 = new Web3(web3.currentProvider);
-        // Acccounts always exposed
-        web3.eth.sendTransaction({/* ... */});
-    }
-    // Non-dapp browsers...
-    else {
+      if (window.ethereum) {
+        window.web3 = new Web3(window.ethereum);
+         try {
+            await window.ethereum.enable();// Request account access if needed
+          } catch (error) {
+           console.log('Please give access to perform transactions.');// User denied account access...
+          }
+      } else if (window.web3) {
+        window.web3 = new Web3(window.web3.currentProvider);
+      }else {
         console.log('Non-Ethereum browser detected. You should consider trying MetaMask!');
-    }
+      }
+  };
+
+  async loadBlockChainData() {
+    let web3 = window.web3 ;
+    const accounts = await web3.eth.getAccounts();
+    const networkId = await web3.eth.net.getId();
+    const deployedNetwork = MarketPlace.networks[networkId];
+    const instance = new web3.eth.Contract(
+      MarketPlace.abi ,
+      deployedNetwork && deployedNetwork.address
+    );
+      
+    this.setState({
+      account: accounts[0],
+      instance: instance
     });
-  }
+  };
 
   render() {
     return (
@@ -56,6 +65,7 @@ class App extends Component {
             <main role="main" className="col-lg-12 d-flex text-center">
               <div className="content mr-auto ml-auto">
                 <h1>Market Place</h1>
+                <p> Current User : { this.state.account}</p>
                 <p>
                   Earn some money with your used stuff !!!
                 </p>
